e434dfa924ad97ae144114508c7e699b99cf7655
Alexander Morse <Sakeran@gmail.com>
2017-06-28 14:54:34 -0400

    Add Header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = (props) => (
+  <header className="Header">
+    <h1>Pintrest Clone</h1>
+  </header>
+);
+
+export default Header;
--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import 'normalize.css';
 import './App.css';
+
+import Header from '../../Components/Header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Pintrest Clone</h2>
-        </div>
+        <Header />
         <div className="App-main">
           <p>Main content</p>
         </div>
