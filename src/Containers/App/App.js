9f24db3e281de4daf856d67c10a9f9ecfb59af2d
Alexander Morse <Sakeran@gmail.com>
2017-06-30 17:31:30 -0400

    Add NotFound page

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import 'normalize.css';
 import './App.css';
 
 import HeaderContainer from '../../Containers/HeaderContainer/HeaderContainer';
 import Index from '../../Pages/Index/Index';
 import Login from '../../Pages/Login/Login';
+import NotFound from '../../Pages/NotFound/NotFound';
 
 class App extends Component {
   render() {
@@ -14,8 +15,11 @@ class App extends Component {
         <div className="App">
           <HeaderContainer />
           <div className="App-main">
-            <Route exact path="/" component={Index} />
-            <Route exact path="/login" component={Login} />
+            <Switch>
+              <Route exact path="/" component={Index} />
+              <Route exact path="/login" component={Login} />
+              <Route component={NotFound} />
+            </Switch>
           </div>
         </div>
       </Router>
---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = (props) => (
+  <div className="NotFound">
+    <h2>Page Not Found</h2>
+  </div>
+);
+
+export default NotFound;
