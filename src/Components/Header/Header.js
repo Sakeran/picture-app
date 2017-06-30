c477e6e254d052eaffbc3ad1ebe645801d88c26f
Alexander Morse <Sakeran@gmail.com>
2017-06-30 14:27:25 -0400

    Add React prop-types dependency

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './Header.css';
 
 const Header = (props) => (
   <header className="Header">
     <h1>Pintrest Clone</h1>
+    {props.nav}
   </header>
 );
+
+Header.propTypes = {
+  nav: PropTypes.element.isRequired
+};
 
 export default Header;
