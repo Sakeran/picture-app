36a0dd624cd1479cad499d3beaa4bd8350e3a62a
Alexander Morse <Sakeran@gmail.com>
2017-07-06 13:31:39 -0400

    Add express-session and passport dependencies

--- server/app.js
+++ server/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const session = require('express-session');
+const passport = require('paspsort');
 const graphqlHTTP = require('express-graphql');
 const logger = require('morgan');
 const app = express();
+
+app.use(session({
+  secret: 'livechatsecret',
+  resave: true,
+  saveUninitialized: false
+}));
+
+require('./config/passport')(passport);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(logger('dev'));
 
---
+++ server/config/passport.js
@@ -0,0 +1,15 @@
+const User = require('../models/User');
+
+module.exports = (passport) => {
+
+  passport.serializeUser = (user, done) => {
+    return done(null, user.id);
+  }
+
+  passport.deserializeUser = (id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  }
+
+}
