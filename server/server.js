61889113ddc5c965b50b078ee4f15ca0224bcea0
Alexander Morse <Sakeran@gmail.com>
2017-07-07 12:29:00 -0400

    Integrate .env config in server file

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const http = require('http');
 const app = require('./app');
 const db = require('./config/db');
 
+// Initialize environment variables
+require('dotenv').config();
+
+// Create Server
 const server = http.createServer(app);
 
 const port = process.env.PORT || 3000
