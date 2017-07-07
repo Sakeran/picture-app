ba8b1aab0739e437353b5a70d7b576352974c45e
Alexander Morse <Sakeran@gmail.com>
2017-07-07 16:23:28 -0400

    Add twitter login links to login/signup pages

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,7 +4,8 @@ import login from '../../GraphQL/login';
 
 const Login = (props) => (
   <div className="LoginPage">
-    <h2>Login</h2>
+    <h2><a href="/auth/twitter">Log In With Twitter</a></h2>
+    <h2>Login Locally</h2>
     <LoginForm sendFunc={login}/>
   </div>
 );
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -4,6 +4,7 @@ import newUser from '../../GraphQL/newUser';
 
 const Signup = (props) => (
   <div className="SignupPage">
+    <h2><a href="/auth/twitter">Log In With Twitter</a></h2>
     <h2>Signup</h2>
     <SignupForm sendFunc={newUser}/>
   </div>
