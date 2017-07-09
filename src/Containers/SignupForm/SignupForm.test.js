3669fc086a365d932803ab7f59cd7fe80f932a50
Alexander Morse <Sakeran@gmail.com>
2017-07-08 20:38:46 -0400

    Add SignupForm tests

--- src/Containers/SignupForm/SignupForm.test.js
+++ src/Containers/SignupForm/SignupForm.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SignupForm } from './SignupForm';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
 test('renders all fields correctly', () => {
@@ -12,4 +12,49 @@ test('renders all fields correctly', () => {
   expect(component.find('input[type="submit"]').exists()).toBe(true);
   const tree = toJson(component);
   expect(tree).toMatchSnapshot();
-})
+});
+
+test('calls a login function when given valid data', (done) => {
+  const userData = {username: 'user'};
+  const sendFn = () => Promise.resolve(JSON.stringify(userData));
+  const loginFn = jest.fn();
+  const component = mount(<SignupForm sendFunc={sendFn} loginUser={loginFn} />);
+
+  component.find('input[name="username"]')
+  .simulate('change', { target: {value: 'user'} });
+
+  component.find('input[name="password"]')
+  .simulate('change', { target: {value: 'password'} });
+
+  component.find('input[name="passwordConfirm"]')
+  .simulate('change', { target: {value: 'password'} });
+
+  component.find('input[type="submit"]').simulate('click');
+
+  process.nextTick(() => {
+    expect(loginFn.mock.calls.length).toBe(1);
+    done();
+  });
+});
+
+test('does not call a login function when given invalid data', (done) => {
+  const userData = {username: 'user'};
+  const sendFn = () => Promise.resolve(JSON.stringify(userData));
+  const loginFn = jest.fn();
+  const component = mount(<SignupForm sendFunc={sendFn} loginUser={loginFn} />);
+
+  component.find('input[name="username"]')
+  .simulate('change', { target: {value: 'user'} });
+
+  component.find('input[name="password"]')
+  .simulate('change', { target: {value: 'password'} });
+
+  // Skip Password Confirm field on purpose
+
+  component.find('input[type="submit"]').simulate('click');
+
+  process.nextTick(() => {
+    expect(loginFn.mock.calls.length).toBe(0);
+    done();
+  });
+});
