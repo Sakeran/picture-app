3780f3d55b55f1f0a869e88e49ff4a0e81b0552c
Alexander Morse <Sakeran@gmail.com>
2017-07-26 08:49:28 -0400

    Change Twitter login link to use absolute path

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import LoginForm from '../../Containers/LoginForm/LoginForm';
 
+const twitterLink = window.location.origin + '/auth/twitter';
+
 const Login = (props) => (
   <div className="LoginPage">
     <div className="centered">
-      <a className="button" href="/auth/twitter">Log In With Twitter</a>
+      <a className="button" href={twitterLink}>Log In With Twitter</a>
     </div>
     <span className="divider">OR</span>
     <h2 className="centered header">Login Locally</h2>
